221b446867a0c4b33abfd9d870b1c03538062bd8
vats <shivamvats1610@gmail.com>
2024-04-22 21:16:50 +0530

    Coupon-uthao using react, Nodejs
    React, Nodejs, Express, MongoDB

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Layout from "./js/components/Layout";
+import SearchContentContainer from "./js/containers/SearchContentContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SearchContentContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+// import ReactDOM from "react-dom";
+import { createRoot} from 'react-dom/client';
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import {  legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./js/reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const couponStore = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+// ReactDOM.render(
+//   <Provider store={couponStore}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={couponStore}>
+     <App />
+   </Provider>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
